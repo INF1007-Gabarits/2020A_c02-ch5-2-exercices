11745c264233c637d8e6db833387b08c0293597a
Charles Hosson <charles.hosson@polymtl.ca>
2020-09-24 16:13:56 -0400

    Removed answers.

--- exercice.py
+++ exercice.py
@@ -4,36 +4,21 @@
 import math
 
 def get_num_letters(text):
-	return len([None for chr in text if chr.isalnum()])
+	return 0
 
 def get_word_length_histogram(text):
-	histogram = [0]
-	for word in text.split():
-		length = get_num_letters(word)
-		if length >= len(histogram):
-			histogram += [0] * (length - len(histogram) + 1)
-		histogram[length] += int(length != 0)
-	return histogram
+	return [0]
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
 
-	number_alignment = len(str(len(histogram) - 1))
-	result = "\n".join([f"{i+1 : >{number_alignment}} {ROW_CHAR * elem}" for i, elem in enumerate(histogram[1:])])
-	return result
+	return ""
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
 
-	max_length = max(histogram)
-	result = ""
-	for row in range(max_length, 0, -1):
-		for elem in histogram[1:]:
-			result += BLOCK_CHAR if elem >= row else " "
-		result += "\n"
-	result += LINE_CHAR * len(histogram)
-	return result
+	return ""
 
 
 if __name__ == "__main__":
